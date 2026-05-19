99caec77406052364f5d2c1954dafce569e2534e
ruandiegl <ruandiegl33@gmail.com>
2026-05-19 10:20:00 -0300

    url do fetch

--- web/curriculos_project/src/services/api.ts
+++ web/curriculos_project/src/services/api.ts
@@ -3,8 +3,9 @@ import axios from 'axios';
 const TOKEN_STORAGE_KEY = 'token';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:3001',
+  baseURL: 'http://192.168.71.130:3001',
 });
+console.log(import.meta.env.VITE_API_URL)
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem(TOKEN_STORAGE_KEY);
